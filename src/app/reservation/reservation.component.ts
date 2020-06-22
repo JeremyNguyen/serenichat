98902c086f1c3ff7849e2681600f5e876a5fcbb5
Jeremy Nguyen <nguyen.jeremy.minhchau@gmail.com>
2020-06-22 23:48:55 +0200

    Debut reservation

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import moment from 'moment';
 
 @Component({
   selector: 'app-reservation',
@@ -7,9 +9,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationComponent implements OnInit {
 
-  constructor() { }
+  startOfWeek: Date;
+  endOfWeek: Date;
 
-  ngOnInit(): void {
+  creneaux = {
+    0: null,
+    1: null,
+    2: ['9:00', '10:30', '12:00', '13:30', '15:00', '16:30', '18:00'],
+    3: ['9:00', '14:00'],
+    4: ['9:00', '10:30', '12:00', '13:30', '15:00', '16:30', '18:00'],
+    5: ['9:00', '10:30', '12:00', '13:30', '15:00', '16:30', '18:00'],
+    6: ['9:00', '14:00'],
+  };
+
+  dureeSeance = {
+    0: null,
+    1: null,
+    2: 1,
+    3: 4,
+    4: 1,
+    5: 1,
+    6: 4
+  };
+
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.startOfWeek = moment().startOf('week').add('d', 1).toDate();
+    this.endOfWeek = moment().endOf('week').add('d', 1).toDate();
+  }
+
+  debug() {
+/*    console.log(this.startOfWeek);
+    console.log(this.endOfWeek);
+    console.log(this.getWeek());*/
+
+    for (const day of this.getWeek()) {
+      console.log(day);
+      console.log(this.getCreneauxForDay(day));
+      console.log(this.getDureeSeanceForDay(day));
+    }
+
+  }
+
+  getCreneauxForDay(day: Date) {
+    return this.creneaux[day.getDay()];
+  }
+
+  getDureeSeanceForDay(day: Date) {
+    return this.dureeSeance[day.getDay()];
+  }
+
+  getWeek() {
+    const week = [];
+    week.push();
+    for (let i = 0; i < 7; i++) {
+      week.push(moment(this.startOfWeek).add('d', i).toDate());
+    }
+    return week;
+  }
+
+  addDays(days: number) {
+    this.startOfWeek = moment(this.startOfWeek).add('d', days).toDate();
+    this.endOfWeek = moment(this.endOfWeek).add('d', days).toDate();
+  }
 }
