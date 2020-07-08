3832b44cfbf7153be558c2f4c463d9a350bde836
Jeremy Nguyen <nguyen.jeremy.minhchau@gmail.com>
2020-07-08 23:19:59 +0200

    Uncomment backend calls

--- src/commons/services/reservation.service.ts
+++ src/commons/services/reservation.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {SeanceDto} from '../dtos/seanceDto';
 import {FormulaireDto} from '../dtos/formulaireDto';
 import {Observable} from 'rxjs';
@@ -11,31 +11,31 @@ export class ReservationService {
 
   private readonly HEADERS: HttpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
 
-  private readonly URL_RESERVATIONS = 'http://127.0.0.1:3000/reservations';
+  private readonly URL_RESERVATIONS = 'http://127.0.0.1:3000/seances';
 
   constructor(private httpClient: HttpClient) {
   }
 
   getReservations(start: Date, end: Date): Observable<any[]> {
     // TODO
-    return new Observable((observer) => {
+/*    return new Observable((observer) => {
       observer.next([]);
       observer.complete();
-    });
-/*    const params = new HttpParams();
+    });*/
+    const params = new HttpParams();
     params.set('start', start.toString());
     params.set('end', end.toString());
-    return this.httpClient.get<any[]>(this.URL_RESERVATIONS, {headers: this.HEADERS, params});*/
+    return this.httpClient.get<any[]>(this.URL_RESERVATIONS, {headers: this.HEADERS, params});
   }
 
-  reserver( formulaire: FormulaireDto) {
+  reserver(formulaire: FormulaireDto) {
     // TODO
-    console.log(formulaire);
+/*    console.log(formulaire);
     return new Observable((observer) => {
       observer.next([]);
       observer.complete();
-    });
-    // return this.httpClient.post(this.URL_RESERVATIONS, null, {headers: this.HEADERS});
+    });*/
+    return this.httpClient.post(this.URL_RESERVATIONS, formulaire, {headers: this.HEADERS});
   }
 
 }
