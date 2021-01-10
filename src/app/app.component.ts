0c007243d809a8cdcb2220ad7856eb088c577eb6
Jeremy Nguyen <nguyen.jeremy.minhchau@gmail.com>
2021-01-10 21:02:19 +0100

    Added googleanalytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          gtag('config', 'G-FLY3WHYKVV',
+            {
+              'page_path': event.urlAfterRedirects
+            }
+          );
+        }
+      }
+    );
+  }
 }
